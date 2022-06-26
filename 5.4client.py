055554e3ea8fe271ae746007df4a85e4c58391f2
atifahz <2020489494@student.uitm.edu.my>
2022-06-26 12:59:13 +0000

    Lab 6

---
+++ 5.4client.py
@@ -0,0 +1,10 @@
+from netmiko import ConnectHandler
+
+myserver = {
+ 'device_type':'linux',
+ 'host':'192.168.178.15', #Your Server IP
+ 'username':'atifahzakaria', #Your Server Username
+ 'password':'naz', #Your Server Password
+ 'port': 22,
+ 'secret':'',
+}
